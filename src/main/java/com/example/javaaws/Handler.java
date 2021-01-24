c72d72f222732cc4e34b2edb3ef1badd86ddc533
Heitor Boschirolli <heitor.boschirolli@gmail.com>
2021-01-24 08:52:27 -0300

    Add joda time dependency

--- src/main/java/com/example/javaaws/Handler.java
+++ src/main/java/com/example/javaaws/Handler.java
@@ -2,11 +2,12 @@ package com.example.javaaws;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.joda.time.DateTime;
 
 import java.util.Map;
 
 public class Handler implements RequestHandler<Map<String,String>, String> {
     public String handleRequest(Map<String, String> event, Context context) {
-        return "Hello there";
+        return DateTime.now().toString();
     }
 }
