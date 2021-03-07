b0e00494540ad03122da2a1185c497be4c4da124
Heitor Boschirolli <heitor.boschirolli@gmail.com>
2021-03-07 19:00:35 -0300

    Add joda time

--- src/main/java/com/example/skidae/Handler.java
+++ src/main/java/com/example/skidae/Handler.java
@@ -2,11 +2,12 @@ package com.example.skidae;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.joda.time.DateTime;
 
 import java.util.Map;
 
 public class Handler implements RequestHandler<Map<String, String>, Map<String, String>>  {
     public Map<String, String> handleRequest(Map<String, String> input, Context context) {
-        return Map.of("Hello", "there");
+        return Map.of("Time", DateTime.now().toString());
     }
 }
