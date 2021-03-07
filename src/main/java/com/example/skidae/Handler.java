73db1e26a8023bcc60bac1dc0d22a4443b63dc2c
Heitor Boschirolli <heitor.boschirolli@gmail.com>
2021-03-07 18:42:35 -0300

    Clean up

---
+++ src/main/java/com/example/skidae/Handler.java
@@ -0,0 +1,12 @@
+package com.example.skidae;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import java.util.Map;
+
+public class Handler implements RequestHandler<Map<String, String>, Map<String, String>>  {
+    public Map<String, String> handleRequest(Map<String, String> input, Context context) {
+        return Map.of("Hello", "there");
+    }
+}
